2da4c467e55f620650c238cd3f2ddd3baba0642b
andrewipark <andrewipark@users.noreply.github.com>
2017-09-26 19:17:36 -0500

    oops

--- hw3_test.py
+++ hw3_test.py
@@ -25,7 +25,7 @@ def test_search_2d_array():
 
 def test_matrix_transpose():
 	assert matrix_transpose([[1]]) == [[1]]
-	assert matrix_transpose([[1, 2, 3],[4, 5, 6],[7, 8, 9]]) == [[1, 4, 7], [2, 5, 8], [3, 6, 9]])
+	assert matrix_transpose([[1, 2, 3],[4, 5, 6],[7, 8, 9]]) == [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
 	assert matrix_transpose([[1, 2], [3, 4], [5, 6], [7, 8]]) == [[1, 3, 5, 7], [2, 4, 6, 8]]
 
 def test_binary_search():
